1e260096bea3da5023ea5c43617bb43de41180ef
Sam Heather <sam@heather.sh>
2013-11-02 21:07:15 +0000

    other changes

--- threads/threads.c
+++ threads/threads.c
@@ -6,21 +6,26 @@
 
 struct thread_info {    /* Used as argument to thread_start() */
 	pthread_t thread_id;/* ID returned by pthread_create() */
-//	int thread_num;     /* Application-defined thread # */
-//	char *argv_string;  /* From command-line argument */
+	char passedChar[1];
+	pthread_mutex_t passedCharMutex;
+	pthread_cond_t conditionalSignal;
 };
 
 static void *thread_1_start(void *arg) {
 	struct thread_info *myInfo = arg;
-	printf("Thread id: %d\n", myInfo->thread_id);
+	printf("Started thread id: %d\n", myInfo->thread_id);
+	while (1) {
+		read(0,
+	}
 	pthread_exit(0);
 }
 
 int main() {
-	pthread_attr_t attr;
 	struct thread_info tinfo;
+	tinfo.thread_id=1234;
 
 	int s;
+	printf("Main thread id: %d\n", tinfo.thread_id);
 	s = pthread_create(&tinfo.thread_id,
 		NULL, // was address of attr, error as this was not initialised.
 		&thread_1_start,
