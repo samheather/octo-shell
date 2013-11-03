1482cd94fa99eea7c1a723aaa668d916b44bdde4
Sam Heather <sam@heather.sh>
2013-11-03 19:54:09 +0000

    added mutex testing, not tested

--- threads/threads.c
+++ threads/threads.c
@@ -13,22 +13,31 @@ struct thread_info {    /* Used as argument to thread_start() */
 
 static void *thread_1_start(void *arg) {
 	struct thread_info *myInfo = arg;
-	printf("Started thread id: %d\n", myInfo->thread_id);
+	//printf("Started thread id: %d\n", myInfo->thread_id);
+	
 	while (1) {
-		read(0,
+		pthread_mutex_lock(myInfo->passedCharMutex);
+		int rid = read(0,myInfo->passedChar,1);
+		pthread_mutex_unlock(myInfo->passedCharMutex);
 	}
-	pthread_exit(0);
+	
+pthread_exit(0);
 }
 
 int main() {
 	struct thread_info tinfo;
-	tinfo.thread_id=1234;
 
-	int s;
-	printf("Main thread id: %d\n", tinfo.thread_id);
-	s = pthread_create(&tinfo.thread_id,
+	//printf("Main thread id: %d\n", tinfo.thread_id);
+	
+	int s = pthread_create(&tinfo.thread_id,
 		NULL, // was address of attr, error as this was not initialised.
 		&thread_1_start,
 		&tinfo);
 	pthread_join(tinfo.thread_id,NULL);
+
+	while (1) {
+		pthread_mutex_lock(&tinfo.passedCharMutex);
+		write(1,tinfo.passedChar,1);
+		pthread_mutex_unlock(&tinfo.passedCharMutex);		
+	}
 }
