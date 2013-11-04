b814e953058609dc1643591b21a1bd444747aa09
Sam Heather <sam@heather.sh>
2013-11-04 20:23:46 +0000

    more progress to threads

--- threads/threads.c
+++ threads/threads.c
@@ -7,13 +7,13 @@
 struct thread_info {    /* Used as argument to thread_start() */
 	pthread_t thread_id;/* ID returned by pthread_create() */
 	char passedChar[1];
-	pthread_mutex_t passedCharMutex;
+	pthread_mutex_t passedCharMutex = PTHREAD_MUTEX_INITIALIZER;
 	pthread_cond_t conditionalSignal;
 };
 
 static void *thread_1_start(void *arg) {
 	struct thread_info *myInfo = arg;
-	//printf("Started thread id: %d\n", myInfo->thread_id);
+	printf("Started thread id: %d\n", myInfo->thread_id);
 	
 	while (1) {
 		pthread_mutex_lock(myInfo->passedCharMutex);
@@ -27,7 +27,7 @@ pthread_exit(0);
 int main() {
 	struct thread_info tinfo;
 
-	//printf("Main thread id: %d\n", tinfo.thread_id);
+	printf("Main thread id: %d\n", tinfo.thread_id);
 	
 	int s = pthread_create(&tinfo.thread_id,
 		NULL, // was address of attr, error as this was not initialised.
