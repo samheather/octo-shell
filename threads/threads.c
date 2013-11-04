0295ff16c2ee72f12500ba747c8a4d7b13198526
Sam Heather <sam@heather.sh>
2013-11-04 20:18:04 +0000

    progress

--- threads/threads.c
+++ threads/threads.c
@@ -6,28 +6,26 @@
 
 struct thread_info {    /* Used as argument to thread_start() */
 	pthread_t thread_id;/* ID returned by pthread_create() */
-	char passedChar[1];
+	char passedChar[2];
 	pthread_mutex_t passedCharMutex;
 	pthread_cond_t conditionalSignal;
 };
 
 static void *thread_1_start(void *arg) {
 	struct thread_info *myInfo = arg;
-	//printf("Started thread id: %d\n", myInfo->thread_id);
-	
+	printf("Started thread id: %d\n", myInfo->thread_id);	
 	while (1) {
-		pthread_mutex_lock(myInfo->passedCharMutex);
-		int rid = read(0,myInfo->passedChar,1);
-		pthread_mutex_unlock(myInfo->passedCharMutex);
+		printf("thread1 while ran");
+		pthread_mutex_lock(&myInfo->passedCharMutex);
+		int rid = read(0,myInfo->passedChar,2);
+		pthread_mutex_unlock(&myInfo->passedCharMutex);
 	}
-	
-pthread_exit(0);
+	pthread_exit(0);
 }
 
 int main() {
 	struct thread_info tinfo;
-
-	//printf("Main thread id: %d\n", tinfo.thread_id);
+	printf("Main thread id: %d\n", tinfo.thread_id);
 	
 	int s = pthread_create(&tinfo.thread_id,
 		NULL, // was address of attr, error as this was not initialised.
@@ -36,8 +34,9 @@ int main() {
 	pthread_join(tinfo.thread_id,NULL);
 
 	while (1) {
+		printf("thread2 while ran");
 		pthread_mutex_lock(&tinfo.passedCharMutex);
-		write(1,tinfo.passedChar,1);
+		write(1,tinfo.passedChar,2);
 		pthread_mutex_unlock(&tinfo.passedCharMutex);		
 	}
 }
