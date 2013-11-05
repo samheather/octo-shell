9c9eaf70b282acb22ec12b19f1f81fd562337a13
Sam Heather <sam@heather.sh>
2013-11-05 15:33:14 +0000

    start of dining philosophers

--- threads/threads.c
+++ threads/threads.c
@@ -11,19 +11,21 @@ struct thread_info {    /* Used as argument to thread_start() */
 	pthread_mutex_t passedCharMutex;
 	pthread_cond_t readyToReadSignal;
 	pthread_cond_t readyToWriteSignal;
+	pthread_cond_t readyToReadWriteSignal;
 };
 
 static void *thread_1_start(void *arg) {
 	struct thread_info *myInfo = arg;
 	printf("Started thread id: %d\n", (int)myInfo->thread_id);	
 	while (1) {
-		printf("thread1 while ran");
+		//printf("Child thread while ran");
 		pthread_mutex_lock(&myInfo->passedCharMutex);
-		while(myInfo->full) { pthread_cond_wait(&myInfo->readyToReadSignal, &myInfo->passedCharMutex); }
+		while(myInfo->full) { pthread_cond_wait(&myInfo->readyToReadWriteSignal, 
+												&myInfo->passedCharMutex); }
 		int rid = read(0,myInfo->passedChar,2);
 		myInfo->full = 1;
 		pthread_mutex_unlock(&myInfo->passedCharMutex);
-		pthread_cond_signal(&myInfo->readyToWriteSignal);
+		pthread_cond_signal(&myInfo->readyToReadWriteSignal);
 	}
 	pthread_exit(0);
 }
@@ -32,6 +34,7 @@ int main() {
 	struct thread_info tinfo = {.passedCharMutex = PTHREAD_MUTEX_INITIALIZER,
 								.readyToReadSignal = PTHREAD_COND_INITIALIZER,
 								.readyToWriteSignal = PTHREAD_COND_INITIALIZER,
+								.readyToReadWriteSignal = PTHREAD_COND_INITIALIZER,
 								.full = 0};
 	printf("Main thread id: %d\n", (int)tinfo.thread_id);
 
@@ -42,12 +45,13 @@ int main() {
 	//pthread_join(tinfo.thread_id,NULL);
 
 	while (1) {
-		printf("thread2 while ran");
+		//printf("Main thread while ran");
 		pthread_mutex_lock(&tinfo.passedCharMutex);
-		while (tinfo.full == 0) { pthread_cond_wait(&tinfo.readyToWriteSignal, &tinfo.passedCharMutex); }
+		while (tinfo.full == 0) { pthread_cond_wait(&tinfo.readyToReadWriteSignal,
+													&tinfo.passedCharMutex); }
 		write(1,tinfo.passedChar,2);
 		tinfo.full = 0;
 		pthread_mutex_unlock(&tinfo.passedCharMutex);
-		pthread_cond_signal(&tinfo.readyToReadSignal);
+		pthread_cond_signal(&tinfo.readyToReadWriteSignal);
 	}
 }
