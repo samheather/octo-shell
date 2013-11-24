f6610439ffea3d970c44ab5a154af72aa89ab319
Sam Heather <sam@heather.sh>
2013-11-24 18:42:18 +0000

    mostly working buddy memory allocation system

--- buddyMemoryAllocation.c
+++ buddyMemoryAllocation.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 
 int globalMemorySize;
 void* global;
@@ -10,63 +12,72 @@ struct node {
 	int status; // 0 = empty, 1 = has children, 2 = node full
 };
 
+int findAvailable(int requestedSpace, struct node *memoryMapNode, void* returnedPointer, int calls) {
+	int currentNodeSpace = globalMemorySize/pow(2,(calls-1));
+	printf("Called with callid: %d, nodeStatus: %d, memoryRequested: %d, currentNodeSpace: %d  << WHY 40???? \n",
+		calls, memoryMapNode->status, requestedSpace, currentNodeSpace);
+	// If node full, or requested > node size, return error.
+	if (memoryMapNode->status == 2 | requestedSpace > currentNodeSpace) {
+		return 1;
+	}
+	// Else if node empty, and appropriate size, allocate memory.
+	else if (memoryMapNode->status == 0) {
+		if (requestedSpace > currentNodeSpace/2) {
+			memoryMapNode->status = 2;
+			return 0;
+		}
+		// else if node is too big, create left and right and then recurse on left.
+		else {
+			// create left and right nodes in tree
+			struct node newLeftNode = {.left = 0, .right = 0, .status = 0};
+			struct node newRightNode = {.left = 0, .right = 0, .status = 0};
+			struct node* newLeftNodePointer = &newLeftNode;
+			struct node* newRightNodePointer = &newRightNode; // This + prev 3 - valid, or memory lost?
+			memoryMapNode->left = newLeftNodePointer;
+			memoryMapNode->right = newRightNodePointer;
+			// Adjust memoryMapNode status
+			memoryMapNode->status = 1;
+			// Fill from left first.
+			return findAvailable(requestedSpace, memoryMapNode->left, returnedPointer, calls+1);
+		}
+	}
+	else if (memoryMapNode->status == 1) {
+		if (findAvailable(requestedSpace, memoryMapNode->left, returnedPointer, calls+1) == 1) {
+			if (findAvailable(requestedSpace, memoryMapNode->right, returnedPointer, calls+1) == 1) {
+				printf("went right");
+				return 1;
+			}
+			else {
+				return 0; // this if block can be condensed heavily
+			}
+		}
+		else {
+			printf("went left");
+			return 0;
+		}
+	}
+	else {
+		return 2; // Some unknown error occurred.
+	}
+}
+
 int main() {
 	// First allocate 1Mbyte
 	globalMemorySize = 1048576;
 	global = malloc(globalMemorySize); // 1024^2
 	
-	struct node memoryMap = {.left = nullptr, .right = nullptr, .status = 0};
+	struct node memoryMap = {.left = 0, .right = 0, .status = 0};
+//	printf(memoryMap.left);
+	void* requestedSpace;
+	void* requestedSpace2;
+	findAvailable((1048576/4)-1, &memoryMap, &requestedSpace, 1);
+	findAvailable((1048576/4)-1, &memoryMap, &requestedSpace2, 1);
+//	printf(memoryMap.left);
+	
 	
 // 	struct node globalMap = {}
 // 	globalMap.value = globalMemorySize;
 // 
 // 	while(1) {.left - }
 // 	return 1;
-}
-
-int findAvailable(int requestedSpace, struct node *memoryMapNode, void* returnedPointer, int calls) {
-	int currentNodeSpace = globalMemorySize/(2**(calls-1));
-	// If node full, or requested > node size, return error.
-	if (*memoryMapNode.status == 2 | requestSpace > currentNodeSpace) {
-		return 1;
-	}
-	// Else if node empty, and appropriate size, allocate memory.
-	else if (*memoryMapNode.status == 0) {
-		if (requestedSpace > currentNodeSpace/2) {
-			*memoryMapNode.status = 2;
-			return 0;
-		}
-		// else if node is too big, create left and right and then recurse on left.
-		else {
-			// create left and right nodes in tree
-			struct node* newLeftNodePointer = {.left = nullptr, .right = nullptr, .status = 0};
-			struct node* newRightNodePointer = {.left = nullptr, .right = nullptr, .status = 0};
-			memoryMapNode->left = newLeftNodePointer;
-			memoryMapNode->right = newRightNodePointer;
-			// Adjust memoryMapNode status
-			memoryMapNode->status = 1;
-			// Fill from left first.
-			return findAvailable(requestedSpace, memoryMapNode->left, returnedPointer, calls++);
-	}
-	else if (*memoryMapNode.status == 1) {
-		if (findAvailable(requstedSpace, memoryMapNode->left, returnedPointer, calls++) == 1) {
-			if (findAvailable(requstedSpace, memoryMapNode->right, returnedPointer, calls++) == 1) {
-				return 1;
-			}
-		}
-		else {
-			return 0;
-		}
-	}
-		
-	
-	// If request space > global memory size or node is full, return 1
-	// else if requested space > currentNodeSpace / 2, allocate node
-	// else traverse down tree and find node that will work.
-	
-	
-// 	if (globalMap.value >= requestedSpace) {
-// 		int currentVal = buddyMapNode.value;
-// 		*buddyMapNode.value = null;
-
 }
